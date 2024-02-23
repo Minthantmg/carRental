6d3354b53e33b7bca5e9d473fc07246c7aad5368
Min Thant Mg <minthant180@gmail.com>
2024-02-23 23:01:25 +0630

    navbar and footer
    added nav and footer

--- app/types/index.ts
+++ app/types/index.ts
@@ -3,6 +3,7 @@ import {MouseEventHandler} from "react";
 export interface CustomButtonProps {
     title : String;
     containerStyles? : String;
+    btnType :"button" | "submit"
     handleClick ?:
         MouseEventHandler<HTMLButtonElement>;
 }
