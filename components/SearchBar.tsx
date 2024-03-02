585a91b9dbc173286543b4546587aa6f6d477bd5
minthantmg <minthant180@gmail.com>
2024-03-02 21:47:37 +0630

    updated searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,19 +1,10 @@
-'use client'
-import React, {useState} from 'react';
-import {SearchManufacturer} from "./";
+import React from 'react';
 
 const SearchBar = () => {
-    const [manufacturer , setManuFacturer] = useState("")
-    const handleSearch = () => {}
     return (
-        <form className="searchbar" onSubmit={handleSearch}>
-            <div className="searchbar__item">
-                <SearchManufacturer
-                    manufacturer={manufacturer}
-                    setManuFacturer={setManuFacturer}
-                />
-            </div>
-        </form>
+        <div>
+            
+        </div>
     );
 };
 
