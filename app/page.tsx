a2e379eb10eec0dec112b92f0e8e6fc92e832658
minthantmg <minthant180@gmail.com>
2024-03-03 20:59:30 +0630

    added carCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import {CustomFilter, Hero, SearchBar} from "@/components";
+import {CarCard, CustomFilter, Hero, SearchBar} from "@/components";
 import {fetchCars} from "@/utils";
 import {all} from "axios";
 
@@ -8,7 +8,7 @@ import {all} from "axios";
 export default async function Home() {
     const allCars = await fetchCars()
 
-    const isDataEmpty = !Array.isArray(allCars) || allCars.length <0 || !allCars
+    const isDataEmpty = !Array.isArray(allCars) || allCars.length <1 || !allCars
 
     console.log(allCars)
     return (
@@ -29,11 +29,15 @@ export default async function Home() {
                 </div>
               </div>
 
-                {allCars ? (
+                {!isDataEmpty ? (
                     <section>
-                        we have cars
+                        <div className='home__cars-wrapper'>
+                            {allCars?.map((car) => (
+                                <CarCard car={car}/>
+                            ))}
+                        </div>
                     </section>
-                ):(
+                ) : (
                     <div className="home__error-container">
                         <h2 className="text-black text-xl font-bold">Oops, no results</h2>
                     </div>
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,14 +1,29 @@
 import React, {MouseEventHandler} from "react";
 
 export interface CustomButtonProps {
-    title : String;
-    containerStyles? : String;
-    btnType :"button" | "submit"
-    handleClick ?:
+    title: String;
+    containerStyles?: String;
+    btnType: "button" | "submit"
+    handleClick?:
         MouseEventHandler<HTMLButtonElement>;
 }
 
-export interface  SearchManufacturerProps{
-    manufacturer : String;
-    setManufacturer : (manufacturer:String) => void;
+export interface SearchManufacturerProps {
+    manufacturer: String;
+    setManufacturer: (manufacturer: String) => void;
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
 }
